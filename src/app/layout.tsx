71b17ef8848d6b9ef0594fd4a749076bbac7a968
CarlosHeKj <carloslouise2015@gmail.com>
2024-11-19 19:16:34 -0300

    Definindo tipagem para produto

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,10 +26,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-black`}
       >
         <Navbar />
-        <main className="bg-black h-screen p-16 text-purple-400 text-md">
+        <main className=" h-screen p-16 text-purple-400 text-md">
 
         {children}
         </main>
