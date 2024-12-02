b46ce2282d5b2e9e383ad05034f9aca1891c11e5
CarlosHeKj <carloslouise2015@gmail.com>
2024-12-02 13:21:37 -0300

    atum

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,13 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
-export default clerkMiddleware();
+import { clerkMiddleware } from '@clerk/nextjs/server';
+import {  NextResponse } from 'next/server';
+
 export const config = {
-  publicRoutes: ['/'],
-   
-  matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
-  ],
+  runtime: 'experimental-edge', // 'experimental-edge' para funções Edge
 };
+
+// Middleware para Edge Functions
+export default clerkMiddleware((auth, request) => {
+ 
+ 
+  return NextResponse.next();
+});
