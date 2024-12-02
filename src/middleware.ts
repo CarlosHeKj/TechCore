e76bd942e989261423f7b68940867d05143e61d6
CarlosHeKj <carloslouise2015@gmail.com>
2024-12-02 12:52:35 -0300

    Quase desistindo 2

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,18 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware } from '@clerk/nextjs/server';
+import { NextRequest, NextResponse } from 'next/server';
 
 // Define que a função será executada no Edge
 export const config = {
-  runtime: 'experimental-edge',  // Use 'experimental-edge' para o runtime de funções Edge
+  runtime: 'experimental-edge', // 'experimental-edge' para funções Edge
   publicRoutes: ['/', '/product/(.*)', '/sign-in(.*)', '/sign-up(.*)'],
   matcher: [
-    // Ignorar internals do Next.js e arquivos estáticos
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Sempre executar para rotas da API
     '/(api|trpc)(.*)',
   ],
 };
 
-export default clerkMiddleware();
+// Middleware com tipos explícitos para req e res
+export default function middleware(props: any){
+  // Chamando o Clerk Middleware diretamente e retornando o NextResponse
+  return clerkMiddleware(props);
+}
