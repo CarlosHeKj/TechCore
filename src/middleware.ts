708e6a372d3b5fbb23c2e4b92c257025894e979d
CarlosHeKj <carloslouise2015@gmail.com>
2024-12-02 12:17:31 -0300

    ctim

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,12 +2,12 @@ import { clerkMiddleware } from "@clerk/nextjs/server";
 
 // Define que a função será executada no Edge
 export const config = {
-  runtime: 'edge',  // Essencial para funções Edge
+  runtime: 'experimental-edge',  // Use 'experimental-edge' para o runtime de funções Edge
   publicRoutes: ['/', '/product/(.*)', '/sign-in(.*)', '/sign-up(.*)'],
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
+    // Ignorar internals do Next.js e arquivos estáticos
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
+    // Sempre executar para rotas da API
     '/(api|trpc)(.*)',
   ],
 };
