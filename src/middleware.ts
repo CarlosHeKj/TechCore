a79a37f6318a5657673b3691032ec81c9147962c
CarlosHeKj <carloslouise2015@gmail.com>
2024-12-02 13:17:21 -0300

    atum

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,26 +1,12 @@
-import { clerkMiddleware } from '@clerk/nextjs/server';
-import { NextRequest, NextResponse } from 'next/server';
-
-// Configuração do Edge Runtime
+import { clerkMiddleware } from "@clerk/nextjs/server";
+export default clerkMiddleware();
 export const config = {
-  runtime: 'experimental-edge', // 'experimental-edge' para funções Edge
+  publicRoutes: ['/'],
+   
   matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    '/(api|trpc)(.*)', // Incluir rotas da API
+    // Always run for API routes
+    '/(api|trpc)(.*)',
   ],
 };
-
-// Middleware para Edge Functions
-export default clerkMiddleware(
-  (auth, request, event) => {
-    // Verifique se o usuário está autenticado usando 'auth.userId'
-    
-
-    // Se o usuário estiver autenticado, continua com o fluxo da requisição
-    return NextResponse.next();
-  },
-  {
-    // Aqui você pode passar as opções, se necessário
-    // Exemplo de opções: publicRoutes, etc.
-  }
-);
