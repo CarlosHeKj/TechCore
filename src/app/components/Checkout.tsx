477ab5d9015a7dd8fa7f5364f230657220156d28
CarlosHeKj <carloslouise2015@gmail.com>
2024-12-02 23:36:14 -0300

    Retirando barra final

--- src/app/components/Checkout.tsx
+++ src/app/components/Checkout.tsx
@@ -1,27 +1,15 @@
 'use client'
 import { useCartStore } from "@/store";
-import { useEffect } from "react"
 
 export default function Checkout()
 {
     const cartStore = useCartStore();
-useEffect(() => {   
 
-    fetch('/api/create-payment-intent',{
-        method: 'POST',
-        headers:{
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({items: cartStore.cart,
-            payment_intent_id: cartStore.paymentIntent
-        }),
-    }).then((res) => {return res.json()}).then((data) => {console.log(data.paymentIntent)});
-
-},[cartStore.cart, cartStore.paymentIntent]);
 
     return(
         <div>
-            <h1>Checkout</h1>
+            <h1>Pedido completo, já pode encher o carrinho novamente...</h1>
+            <button onClick={() => cartStore.setCheckout('cart')}>Voltar a comprar</button>
         </div>
     )
 }
